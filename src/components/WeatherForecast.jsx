d77b2796b3c02bf0d70b574c51b1d73e130f745c
benjaminnicolas-teboul <benjtenuts@hotmail.fr>
2025-08-18 23:39:55 +0200

    fix: card size to fit wind in destop

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -7,13 +7,15 @@ const WeatherForecast = ({ daily }) => {
     <div className="md:pl-30  flex flex-col gap-4 mt-4 items-center overflow-x-visible pb-6 md:flex-row md:overflow-x-auto md:justify-center md:pb-10">
       {daily.time.map((date, idx) => (
         <WeatherCard
-          className="shadow-lg animate-fade-in rounded-xl border border-gray-200 hover:shadow-xl hover:scale-[1.03] transition-transform duration-200 bg-white/90 w-64 h-80 p-4 flex flex-col items-center justify-between"
+          className="shadow-lg animate-fade-in rounded-xl border border-gray-200 hover:shadow-xl hover:scale-[1.03] transition-transform duration-200 bg-white/90 w-64 h-80 md:h-96 p-4 flex flex-col items-center justify-between"
           key={date}
           date={date}
           max={daily.temperature_2m_max[idx]}
           min={daily.temperature_2m_min[idx]}
           code={daily.weathercode[idx]}
-           wind={daily.windspeed_10m_max ? daily.windspeed_10m_max[idx] : undefined}
+          wind={
+            daily.windspeed_10m_max ? daily.windspeed_10m_max[idx] : undefined
+          }
         />
       ))}
     </div>
